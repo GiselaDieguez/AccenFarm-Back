cc62a28ef1717b76497a20fa816e5d683d9c4900
Gisela <giselladieguez2@gmail.com>
2023-03-20 00:59:25 -0300

    Update ValidationAmtEgg repository

--- src/main/java/Gisela/demo/repository/iValidationRepository.java
+++ src/main/java/Gisela/demo/repository/iValidationRepository.java
@@ -1,11 +1,9 @@
 package Gisela.demo.repository;
-import Gisela.demo.model.Cash;
 import Gisela.demo.model.Validation;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 
 @Repository
 public interface iValidationRepository extends JpaRepository<Validation, Integer>{
@@ -21,13 +19,15 @@ public interface iValidationRepository extends JpaRepository<Validation, Integer
             "WHERE pb.productid = 1", nativeQuery = true)
     Integer validationAmt();
 
-    @Query(value = "SELECT pb.productamt - ps.productamt - dp.productamt as totaleggs\n" +
-            "    FROM buyproducts pb \n" +
-            "    INNER JOIN sellproducts ps \n" +
-            "    ON pb.operationid = ps.operationid\n" +
-            "    INNER JOIN deleteproducts dp\n" +
-            "    ON pb.operationid = dp.operationid\n" +
-            "    WHERE pb.productid = 2", nativeQuery = true)
+    @Query(value = "SELECT pb.productamt + np.productamt - ps.productamt - dp.productamt as totaleggs\n" +
+            "FROM buyproducts pb \n" +
+            "INNER JOIN sellproducts ps \n" +
+            "ON pb.operationid = ps.operationid\n" +
+            "INNER JOIN deleteproducts dp\n" +
+            "ON pb.operationid = dp.operationid\n" +
+            "INNER JOIN newproducts np\n" +
+            "ON pb.operationid = np.operationid\n" +
+            "WHERE pb.productid = 2", nativeQuery = true)
     Integer validationAmtEgg();
 
     @Query(value = "SELECT SUM(totalcash) as totalcash\n" +
