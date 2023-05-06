952573d7a60fd5cff7b380ffb47d3ce537223dd6
Gisela <giselladieguez2@gmail.com>
2023-05-06 10:33:50 -0300

    Update values in ParametersService.

--- src/main/java/Gisela/demo/service/ParametersService.java
+++ src/main/java/Gisela/demo/service/ParametersService.java
@@ -1,5 +1,6 @@
 package Gisela.demo.service;
 
+import jakarta.annotation.PostConstruct;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.TypedQuery;
@@ -10,10 +11,30 @@ import jakarta.persistence.criteria.Root;
 import Gisela.demo.model.Transactions;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 @Service
 public class ParametersService {
+
+    private Properties properties;
     @PersistenceContext
     private EntityManager entityManager;
+
+    @PostConstruct
+    public void init() {
+        properties = new Properties();
+        try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream("product.properties")) {
+            properties.load(inputStream);
+        } catch (IOException e) {
+            throw new IllegalStateException("Could not load configuration file: product.properties", e);
+        }
+        if(entityManager == null) {
+            throw new IllegalStateException("EntityManager has not been injected");
+        }
+    }
+
 
     public <T, R> R getSingleResultByLike(Class<T> entityClass, Class<R> resultClass, String attributeName, String likeAttribute, String likeValue) {
         CriteriaBuilder builder = entityManager.getCriteriaBuilder();
@@ -27,35 +48,35 @@ public class ParametersService {
     }
 
     public Integer productIdChicken() {
-        return getSingleResultByLike(Transactions.class, Integer.class, "productid", "productnm", "%Bought Chicken%");
+        return getSingleResultByLike(Transactions.class, Integer.class, properties.getProperty("productid"), properties.getProperty("productnm"), properties.getProperty("chickenbought"));
     }
 
     public Integer productIdNewChicken() {
-        return getSingleResultByLike(Transactions.class, Integer.class, "productid", "productnm", "%New Chicken%");
+        return getSingleResultByLike(Transactions.class, Integer.class, properties.getProperty("productid"), properties.getProperty("productnm"), properties.getProperty("chickennew"));
     }
 
     public Integer productIdSoldChicken() {
-        return getSingleResultByLike(Transactions.class, Integer.class, "productid", "productnm", "%Sold Chicken%");
+        return getSingleResultByLike(Transactions.class, Integer.class, properties.getProperty("productid"), properties.getProperty("productnm"), properties.getProperty("chickensold"));
     }
 
     public Integer productIdDropChicken() {
-        return getSingleResultByLike(Transactions.class, Integer.class, "productid", "productnm", "%Drop Chicken%");
+        return getSingleResultByLike(Transactions.class, Integer.class, properties.getProperty("productid"), properties.getProperty("productnm"), properties.getProperty("chickendrop"));
     }
 
     public Integer productIdEgg() {
-        return getSingleResultByLike(Transactions.class, Integer.class, "productid", "productnm", "%Bought Egg%");
+        return getSingleResultByLike(Transactions.class, Integer.class, properties.getProperty("productid"), properties.getProperty("productnm"), properties.getProperty("eggbought"));
     }
 
     public Integer productIdNewEgg() {
-        return getSingleResultByLike(Transactions.class, Integer.class, "productid", "productnm", "%New Egg%");
+        return getSingleResultByLike(Transactions.class, Integer.class, properties.getProperty("productid"), properties.getProperty("productnm"), properties.getProperty("eggnew"));
     }
 
     public Integer productIdSoldEgg() {
-        return getSingleResultByLike(Transactions.class, Integer.class, "productid", "productnm", "%Sold Egg%");
+        return getSingleResultByLike(Transactions.class, Integer.class, properties.getProperty("productid"), properties.getProperty("productnm"), properties.getProperty("eggsold"));
     }
 
     public Integer productIdDropEgg() {
-        return getSingleResultByLike(Transactions.class, Integer.class, "productid", "productnm", "%Drop Egg%");
+        return getSingleResultByLike(Transactions.class, Integer.class, properties.getProperty("productid"), properties.getProperty("productnm"), properties.getProperty("eggdrop"));
     }
 
 }
